169ddc2ecd4a5acbf6c2a8bad3e9b0960044d52c
Jeremiah <jclanc7507@gmail.com>
2022-05-23 21:54:15 -0400

    built schemas, including typeDefs as definined in challenge

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { me } = require('apollo-server-express');
+
+const typeDefs = me`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: bookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const {typeDefs, resolvers} = require('./schemas');
 const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
-
 
 const PORT = process.env.PORT || 3001;
 
